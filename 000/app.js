f2782e8eae8c860c8bf903aaf8259abfc2978ae1
Jake Bladt <jake@jakebladt.com>
2016-11-02 17:05:27 -0400

    feature freeze

--- 000/app.js
+++ 000/app.js
@@ -1,11 +1,34 @@
-$(document).ready(function() {
+var viewmodel = {
+  name: "Frances Serious",
+  todoItems: [{ title: "learn handlebars"}]
+};
+
+function bindAdd() {
+  var input = $('#newTask');
+
+  $(input).keyup(function(event) {
+    if(event.keyCode === 13) {
+      addTodo($(this).val());
+      display();
+      bindAdd();
+    } 
+  });
+}
+
+function display() {
   var src = document.getElementById('todolist_t').innerHTML;
   var template = Handlebars.compile(src);
-  var output = template({
-    name: "Frances Serious",
-    todoItems: [{ title: "Conquer World."}]
-  });
+  var output = template(viewmodel);
 
   var placeholder = document.getElementById('todolist');
   placeholder.innerHTML = output;
+}
+  
+function addTodo(item) {
+  viewmodel.todoItems.push({title: item});
+}
+
+$(document).ready(function() {
+  display();
+  bindAdd();
 });
