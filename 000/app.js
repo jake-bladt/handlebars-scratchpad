0c1a134b3684d57adef37e3eead52afd6977c2b2
Jake Bladt <jake@jakebladt.com>
2016-11-02 16:23:28 -0400

    +world conquest

--- 000/app.js
+++ 000/app.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
   var src = document.getElementById('todolist_t').innerHTML;
   var template = Handlebars.compile(src);
   var output = template({
-    name: "Frances Serious"
+    name: "Frances Serious",
+    todoItems: [{ title: "Conquer World."}]
   });
 
   var placeholder = document.getElementById('todolist');
