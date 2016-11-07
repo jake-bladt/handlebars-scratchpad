898458aee35456646a3a02057faf52458ecbfc2f
Jake Bladt <jake@jakebladt.com>
2016-11-06 19:25:43 -0500

    break out firebase init

---
+++ 001-firebase/scripts/fbase.init.js
@@ -0,0 +1,8 @@
+var config = {
+  apiKey: "AIzaSyAVLtzWB8ezIx4GoV-ZrA62RP11PR2JMAM",
+  authDomain: "hbfb-first.firebaseapp.com",
+  databaseURL: "https://hbfb-first.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "1021840708562"
+};
+firebase.initializeApp(config);
