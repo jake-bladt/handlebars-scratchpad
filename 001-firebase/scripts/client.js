04a6b67e47d7a6df55f64c58d2364cae54fc0561
Jake Bladt <jake@jakebladt.com>
2016-11-06 20:21:11 -0500

    -extra reference

--- 001-firebase/scripts/client.js
+++ 001-firebase/scripts/client.js
@@ -1,3 +1,16 @@
+var config = {
+  apiKey: "AIzaSyAVLtzWB8ezIx4GoV-ZrA62RP11PR2JMAM",
+  authDomain: "hbfb-first.firebaseapp.com",
+  databaseURL: "https://hbfb-first.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "1021840708562"
+};
+firebase.initializeApp(config);
+var db = firebase.database();
+
 $(document).ready(function() {
-  
+  var stepCountsRef = db.ref('/stepcounts');
+  stepCountsRef.on("value", function(snapshot) {
+    console.log(snapshot);
+  });
 });
--- 001-firebase/scripts/fbase.init.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  apiKey: "AIzaSyAVLtzWB8ezIx4GoV-ZrA62RP11PR2JMAM",
-  authDomain: "hbfb-first.firebaseapp.com",
-  databaseURL: "https://hbfb-first.firebaseio.com",
-  storageBucket: "",
-  messagingSenderId: "1021840708562"
-};
-firebase.initializeApp(config);
